461ad62c54da9103aca206ebe6271efb84fe1523
Krystian <krystian.wojcik95@gmail.com>
2021-01-05 15:25:17 +0100

    add Models

---
+++ app/Models/OptymizationsAttributes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OptymizationsAttributes extends Model
+{
+    protected $table = 'optymization_attributes';
+    use HasFactory;
+}

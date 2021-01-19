c72ba5e6a541be68ef1e305bb113e46baca4292d
Krystian <krystian.wojcik95@gmail.com>
2021-01-19 18:50:22 +0100

    add fix field to addClient

--- resources/js/components/ClientAddNew.vue
+++ resources/js/components/ClientAddNew.vue
@@ -6,32 +6,25 @@
                 <h1 class="card-header purple-gradient white-text text-center py-4 h4">
                     <strong>Dodaj klienta</strong>
                 </h1>
-
                 <div class="card-body px-lg-5 py-0">
-
                     <form class="md-form">
                         <div class="col-sm-12">
                                 <mdb-input type="text" id="domain-name" name="domain-name"  label="Domena klienta" icon="globe" small="Podaj domenę klienta" v-model="domainName" outline />
                         </div>
                         <mdb-row class="mb-3">
                             <mdb-col class="form-check" col="4">
-                                   <mdb-input type="radio" id="domain-ssl" name="domain-ssl" radioValue="true" v-model="domainSSLon" label="SSL" :checked="domainSSLon == true"/>
-                                   <mdb-input type="radio" id="domain-no-ssl" name="domain-ssl" radioValue="false" v-model="domainSSLoff" label="Brak SSL" :checked="domainSSLoff == false"/>
+                                <mdb-input type="checkbox" id="domain-ssl" name="domain-ssl" v-model="domainSSL" label="SSL ?" />
                             </mdb-col>
                             <mdb-col class="form-check" col="4">
-                                    <mdb-input type="radio" id="domain-www" name="domain-www" radioValue="true"
-                                               v-model="domainWWWon" label="WWW"/>
-                                    <mdb-input type="radio" id="domain-no-www" name="domain-www" radioValue="false"
-                                               v-model="domainWWWoff" label="Brak WWW"/>
+                                <mdb-input type="checkbox" id="domain-www" name="domain-www" v-model="domainWWW" label="WWW ?" />
                             </mdb-col>
                             <mdb-col class="form-check" col="4">
-                                <mdb-input type="checkbox" id="domain-active" name="domain-active" v-model="domainActive" label="Aktywna?" />
+                                <mdb-input type="checkbox" id="domain-active" name="domain-active" v-model="domainActive" label="Aktywna ?" />
                             </mdb-col>
                         </mdb-row>
                         <mdb-row class="justify-content-md-center mt-4">
                             <mdb-btn color="success" class="btn" type="submit">Dodaj klienta</mdb-btn>
                         </mdb-row>
-
                     </form>
                 </div>
             </div>
@@ -52,24 +45,42 @@ export default {
     },
     data() {
         return {
-            domainSSLon: true,
-            domainSSLoff: true,
-            domainWWWon: '',
-            domainWWWoff: '',
+            domainSSL: false,
+            domainWWW: false,
             domainName: '',
             domainActive: true
         };
     },
     watch: {
         domainName: function() {
-            const word = 'https';
-            console.log(this.domainName.includes(word));
-            if(this.domainName.includes(word)) {
-                this.domainSSLon = true;
-                this.domainSSLoff = false;
+            const ssl = 'https';
+            const www = 'www.';
+            if(this.domainName.includes(ssl)) {
+                this.domainSSL = true;
             } else {
-                this.domainSSLon = false;
-                this.domainSSLoff = true;
+                this.domainSSL = false;
+            }
+            if(this.domainName.includes(www)) {
+                this.domainWWW = true;
+            } else {
+                this.domainWWW = false;
+            }
+        },
+
+        domainSSL: function () {
+            console.log('zmiana SSL');
+            if(this.domainSSL) {
+                this.domainName = this.domainName.replace('http://', 'https://');
+            } else {
+                this.domainName = this.domainName.replace('https://', 'http://');
+            }
+        },
+        domainWWW: function () {
+            console.log('zmiana WWW');
+            if(this.domainWWW) {
+                this.domainName = this.domainName.replace('//', '//www.');
+            } else {
+                this.domainName = this.domainName.replace('//www.', '//');
             }
         }
     }

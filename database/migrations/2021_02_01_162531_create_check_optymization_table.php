dfa82b382979c32860d7d3cf096d6334b38af16b
Krystian Wójcik <krystian.wojcik95@gmail.com>
2021-02-01 17:31:29 +0100

    add tables role and check optymization

---
+++ database/migrations/2021_02_01_162531_create_check_optymization_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckOptymizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('check_optymization', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('optymization_id')->unsigned();
+            $table->foreign('optymization_id')->references('id')->on('optymizations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('check_optymization');
+    }
+}
